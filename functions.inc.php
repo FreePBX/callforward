8e63a0d5584da99ea6f00702b90d46c1e1180242
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-03 18:13:40 -0500

    Update license info

--- functions.inc.php
+++ functions.inc.php
@@ -1,24 +1,10 @@
 <?php
 // vim: set ai ts=4 sw=4 ft=php:
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
-// This file is part of FreePBX.
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//  Copyright (C) 2005,2014 Rob Thomas <xrobau@gmail.com>
+//	Copyright 2013 Schmooze Com Inc.
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// Copyright (C) 2005,2014 Rob Thomas <xrobau@gmail.com>
-//
-
 function callforward_get_config($engine) {
 	$modulename = 'callforward';
 
