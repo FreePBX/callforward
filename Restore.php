9ee8fb1dd1731f2d16f79a78025f5c83be52496e
James Finstrom <jfinstrom@sangoma.com>
2018-04-30 18:58:11 +0000

    Fix restore

--- Restore.php
+++ Restore.php
@@ -6,9 +6,11 @@ class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
     $configs = $this->getConfigs();
     $cf = $this->FreePBX->Callforward;
-    foreach($configs as $k => $v){
-      $cf->setMultipleNumberByExten($k,$v['numbers']);
-      $cf->setRingTimerByExtension($k,$v['ringtimer']);
+    foreach($configs as $conf){
+      foreach($conf as $k => $v){
+       $cf->setMultipleNumberByExten($k,$v['numbers']);
+       $cf->setRingTimerByExtension($k,$v['ringtimer']);
+      }
     }
   }
 }
