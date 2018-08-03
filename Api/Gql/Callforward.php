ee90c198e043db1a94a6c9fe36d9f25b8b4f9c2c
Andrew Nagy <andrew.nagy@the159.com>
2018-08-02 21:26:30 -0400

    added api

--- Api/Gql/Callforward.php
+++ Api/Gql/Callforward.php
@@ -8,51 +8,54 @@ use FreePBX\modules\Api\Gql\Base;
 class Callforward extends Base {
 	protected $module = 'callforward';
 
-
-	public function mutationCallback() {
-		if($this->checkAllWriteScope()) {
-		}
-	}
-
-	public function queryCallback() {
-		if($this->checkAllReadScope()) {
-		}
-	}
-
 	public function postInitializeTypes() {
-	}
-	/*
-	public function postInitTypes() {
 		if($this->checkAllReadScope()) {
-			$user = $this->typeContainer->get('user');
-			$user->addField('callforward_unconditional', [
-				'type' => Type::string(),
-				'description' => 'Call Forward No Answer/Unavailable',
-				'resolve' => function($user) {
-					return $this->getNumberByType($user['extension'], 'CFU');
-				}
-			]);
-			$user->addField('callforward_busy', [
-				'type' => Type::string(),
-				'description' => 'Call Forward Busy',
-				'resolve' => function($user) {
-					return $this->getNumberByType($user['extension'], 'CFB');
-				}
-			]);
-			$user->addField('callforward_all', [
-				'type' => Type::string(),
-				'description' => 'Call Forward All',
-				'resolve' => function($user) {
-					return $this->getNumberByType($user['extension'], 'CF');
-				}
-			]);
-			$user->addField('callforward_ringtimer', [
-				'type' => Type::int(),
-				'description' => 'Call Forward Ring Timer',
-				'resolve' => function($user) {
-					return $this->freepbx->Callforward->getRingtimerByExtension($user['extension']);
-				}
-			]);
+			$user = $this->typeContainer->get('coreuser');
+
+			$user->addFieldCallback(function() {
+				return [
+					'callforward_unconditional' => [
+						'type' => Type::string(),
+						'description' => 'Call Forward No Answer/Unavailable',
+						'resolve' => function($user) {
+							if(!isset($user['extension'])) {
+								return null;
+							}
+							return $this->getNumberByType($user['extension'], 'CFU');
+						}
+					],
+					'callforward_busy' => [
+						'type' => Type::string(),
+						'description' => 'Call Forward No Answer/Unavailable',
+						'resolve' => function($user) {
+							if(!isset($user['extension'])) {
+								return null;
+							}
+							return $this->getNumberByType($user['extension'], 'CFU');
+						}
+					],
+					'callforward_all' => [
+						'type' => Type::string(),
+						'description' => 'Call Forward All',
+						'resolve' => function($user) {
+							if(!isset($user['extension'])) {
+								return null;
+							}
+							return $this->getNumberByType($user['extension'], 'CF');
+						}
+					],
+					'callforward_ringtimer' => [
+						'type' => Type::int(),
+						'description' => 'Call Forward Ring Timer',
+						'resolve' => function($user) {
+							if(!isset($user['extension'])) {
+								return null;
+							}
+							return $this->freepbx->Callforward->getRingtimerByExtension($user['extension']);
+						}
+					]
+				];
+			});
 		}
 	}
 
@@ -60,5 +63,5 @@ class Callforward extends Base {
 		$number = $this->freepbx->Callforward->getNumberByExtension($extension, $type);
 		return $number !== false ? $number : '';
 	}
-	*/
+
 }
